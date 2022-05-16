0dabff18e00c90b79bc03ce9e63b66030053b57f
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-16 21:59:25 +0000

    fixed addition assignment

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -34,6 +34,6 @@ console.log('value of isSparta:', isSparta);
 console.log('typeof isSparta:', typeof isSparta);
 
 var motto = fullName;
-motto = fullName + ' is the GOAT';
+motto += ' is the GOAT';
 console.log('value of motto:', motto);
 console.log('typeof motto:', typeof motto);
