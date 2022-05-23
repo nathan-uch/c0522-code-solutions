658d6b334bebcae1aeca70c6b653c7a3766eaa79
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-23 22:29:58 +0000

    NOT COMPLETED

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,19 @@
 /* exported titleCase */
+
+// convert the title into an array separated by spaces
+// assing the array to a new variable title array
+// loop over the title array at each index
+// convert each word into an array, lower case the word, ...
+// and assign it to current word array
+// check if the title array is at index 0...
+// or check if the current word length is more than 4
+// if yes, capitalize first letter
+// check if current word is special case (JavaScript and API)
+// capitalize accordingly
+// check if
+// convert current word array to string inside the title array
+//
+
+function titleCase(title) {
+
+}
