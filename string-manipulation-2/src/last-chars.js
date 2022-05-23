89486cd6f6b99377b7ab07649fbb4209bb37ae10
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-23 20:10:32 +0000

    fixed lastChars extra condition

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -2,8 +2,6 @@
 
 // check if length is greater than string length...
 // if yes, return the full original string
-// check if the length is 1...
-// if yes, return the last letter of the string
 // declare variable to hold final string
 // declare variable for the starting index which is the string length minus given length
 // loop over the string starting at starting index...
@@ -13,8 +11,6 @@
 function lastChars(length, string) {
   if (length > string.length) {
     return string;
-  } else if (length === 1) {
-    return string[string.length - 1];
   }
   var finalString = '';
   var startingIndex = string.length - length;
