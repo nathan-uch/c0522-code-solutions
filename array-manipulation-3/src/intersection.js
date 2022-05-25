85a02589996805ca5d0bf28d0d0b4735e1c5f2e2
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-25 21:38:08 +0000

    Completed Array-manipulation-3

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,19 @@
 /* exported intersection */
+
+// declare variable to hold final array
+// loop through the first original array
+// check if the element at the index is in the second array
+// if yes, add to final array
+// return final array
+
+function intersection(first, second) {
+  var finalArray = [];
+
+  for (let i = 0; i < first.length; i++) {
+    if (second.includes(first[i])) {
+      finalArray.push(first[i]);
+    }
+  }
+
+  return finalArray;
+}
--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,29 @@
 /* exported unique */
+
+// declare variable to hold final array
+// add the first item of the original array to the final array
+// check length of original array, if it's 0 return an empty array
+// loop through original array
+// loop through final array
+// check if the final array includes the element in the original array at index
+// if does not include, add it to the final array
+// return the final array
+
+function unique(array) {
+  var finalArray = [];
+  finalArray.push(array[0]);
+
+  if (array.length === 0) {
+    return [];
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < finalArray.length; j++) {
+      if (!finalArray.includes(array[i])) {
+        finalArray.push(array[i]);
+      }
+    }
+  }
+
+  return finalArray;
+}
