c7116706981d9870056b16704d82445329d3ce51
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-05-27 19:32:49 +0000

    working on optional feature

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -19,7 +19,7 @@ var players = [
   }
 ];
 
-function playRound(players, cards) {
+function createNShuffleDeck() {
   var deck = [];
   var ranks = ['ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'jack', 'queen', 'king'];
   var suits = ['clubs', 'diamonds', 'hearts', 'spades'];
@@ -32,14 +32,20 @@ function playRound(players, cards) {
       deck.push(card);
     }
   }
+
   var shuffled = _.shuffle(deck);
   console.log('cards shuffled', shuffled, 'deck length:', shuffled.length);
+  return shuffled;
+}
+
+function playRound(players, cards) {
+  var deck = createNShuffleDeck();
 
   var winner = players[0];
 
   for (var i = 0; i < players.length; i++) {
     var currentP = players[i];
-    var cardsDealt = shuffled.splice(0, cards);
+    var cardsDealt = deck.splice(0, cards);
     currentP.cards = cardsDealt;
     var points = 0;
     var handConsoleLog = 'Player\'s hand ' + (i + 1) + ': ';
@@ -58,21 +64,31 @@ function playRound(players, cards) {
 
     console.log(handConsoleLog);
 
-    if (winner.points === currentP.points && i !== 0) {
-      console.log('tie breaker');
-    } else if (winner.points < currentP.points) {
+    if (winner.points < currentP.points) {
       winner = currentP;
     }
   }
+
   console.log(players[0].name + ' : ' + players[0].points + ' points');
   console.log(players[1].name + ' : ' + players[1].points + ' points');
   console.log(players[2].name + ' : ' + players[2].points + ' points');
   console.log(players[3].name + ' : ' + players[3].points + ' points');
+
+  for (var w = 0; w < players.length; w++) {
+    if (winner.points === players[w].points && winner.name !== players[w].name) {
+      // winner = tieBreaker(winner, players[w], deck);
+    }
+  }
+
   console.log('Winner:', winner.name + ' with ' + winner.points + ' points!');
 }
 
-// function tieBreaker(player1, player2) {
+// function tieBreaker(player1, player2, remainingDeck) {
+//   console.log('There has been a tie between ' + player1.name + ' and ' + player2.name);
+//   var player1Cards = remainingDeck.splice(0, 2);
+//   var player2Cards = remainingDeck.splice(0, 2);
 
+//   return tieWinner;
 // }
 
-playRound(players, 2);
+playRound(players, 3);
