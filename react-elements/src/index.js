573e5e66eb9d54989cbe8f85941231d2836cdb80
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-07-12 23:26:33 +0000

    Fixed react dom path.

--- react-elements/src/index.js
+++ react-elements/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 const element = React.createElement(
   'h1',
@@ -10,6 +10,5 @@ const element = React.createElement(
 console.log(element);
 
 const container = document.getElementById('root');
-
 const root = ReactDOM.createRoot(container);
 root.render(element);
