ef34899e82546c9259c4115818de9796c58a896d
Nathan <68994507+nathan-uch@users.noreply.github.com>
2022-07-12 22:18:25 +0000

    Completed React-Elements.

---
+++ react-elements/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const element = React.createElement(
+  'h1',
+  null,
+  'Hello, React!'
+);
+
+console.log(element);
+
+const container = document.getElementById('root');
+
+const root = ReactDOM.createRoot(container);
+root.render(element);
